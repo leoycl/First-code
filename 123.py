4375f74e432ccd2510804806591234db748541c6
Leo Lee <yichenglee_leo@outlook.com>
2018-10-03 23:48:48 +0800

    Add new file

---
+++ 123.py
@@ -0,0 +1,7 @@
+
+name = input('Please type your name: ')
+print('Hi', name)
+
+h = input('What is your height?')
+w = input('What is your weight?')
+print('My height is', h, ', and my weight is', w)
